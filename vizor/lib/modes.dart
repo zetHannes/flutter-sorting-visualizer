939710d268eb2ca8393d0eb002286bc44b566154
Hannes <zetHannes@outlook.com>
2020-12-28 21:31:48 +0100

    Add comment to modes.dart

--- vizor/lib/modes.dart
+++ vizor/lib/modes.dart
@@ -1,3 +1,6 @@
+/* These are all sorting modes. Every new sorting algorithm 
+needs an entry(sequential) in this file.
+*/
 final int modeBubbleSort = 0,
             modeQuickSort = 1,
             modeHeapSort = 2,
